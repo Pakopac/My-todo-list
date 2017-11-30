73021b6637d1e51ba926aed297f93cbc81180394
Kevin Badinca <kevin.badinca@supinternet.fr>
2017-11-30 22:54:17 +0100

    Added remove button

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 window.onload = function(){
     document.querySelector('#add').onclick = function(){
         var addcolumns = document.createElement("div");
-        document.body.appendChild(addcolumns);
+        document.getElementById('columns').appendChild(addcolumns);
         addcolumns.classList.add("column");
-    };
-}
+
+        var addTitle = document.createElement("p");
+        var addDelete = document.createElement("button");
+        addcolumns.appendChild(addTitle);
+        addcolumns.appendChild(addDelete);
+        var text = document.createTextNode('titre');
+        var exit = document.createTextNode('X');
+        addTitle.appendChild(text);
+        addDelete.appendChild(exit);
+        addDelete.classList.add("delete");
+
+        function remove() {
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        }
+        var task = document.querySelectorAll('.column');
+        var button = document.querySelectorAll('.delete');
+        for (var i = 0; i < task.length; i++) {
+             button[i].addEventListener('click', remove, false);
+        }
+
+	};
+
+};
