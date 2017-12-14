6c7b4b767fbb662e0eb938af25d72527dfebb092
unknown <lilian.pacaud@supinternet.fr>
2017-12-14 02:08:54 +0100

    debug button Add Table

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function drop(ev) {
     ev.target.appendChild(document.getElementsByClassName(data)[0]);
 }
 window.onload = function(){
-    document.querySelector('#add').onclick = function() {
+    document.querySelector('#add').onclick = function newColomn() {
         var addcolumns = document.createElement("div");
         document.getElementById('columns').appendChild(addcolumns);
         addcolumns.classList.add("column");
@@ -32,10 +32,10 @@ window.onload = function(){
         addcolumns.appendChild(addDelete);
         addcolumns.appendChild(addTitle);
         addcolumns.appendChild(addTable);
-        var text = document.createTextNode('task');
+        var textTask = document.createTextNode('task');
         var exit = document.createTextNode('X');
         var buttonAddTable = document.createTextNode('Add Table');
-        addTitle.appendChild(text);
+        addTitle.appendChild(textTask);
         addDelete.appendChild(exit);
         addTable.appendChild(buttonAddTable);
         addDelete.classList.add("delete");
@@ -51,10 +51,10 @@ window.onload = function(){
             };
         }
 
+
         var btnTable = document.querySelectorAll('.btnAddTable');
         var inputTitle = document.querySelector('.inputTitle');
         var description = document.querySelector('.description');
-        var takeColumn = document.querySelectorAll('.column');
         var task = document.querySelectorAll('.titleTask');
         var btnChange = document.querySelectorAll('.btnChange');
 
@@ -62,40 +62,45 @@ window.onload = function(){
         for (var i = 0; i < btnChange.length; i++){
 
         };
-            btnTable[i].onclick = function NewTable() {
+            addTable.onclick = function NewTable() {
                 var createTable = document.createElement("div");
                 createTable.classList.add("Table");
-                createTable.setAttribute("draggable","true");
-                createTable.setAttribute("ondragstart","drag(event)");
+                createTable.setAttribute("draggable", "true");
+                createTable.setAttribute("ondragstart", "drag(event)");
+
                 var createInputTitle = document.createElement("input");
                 createInputTitle.setAttribute("type", "text");
                 createInputTitle.classList.add("inputTitle");
-                takeColumn[i].appendChild(createTable);
+                addcolumns.appendChild(createTable);
                 createTable.appendChild(createInputTitle);
+
                 var createdescription = document.createElement("textarea");
                 createdescription.classList.add("inputDescription");
                 createTable.appendChild(createdescription);
+
                 var createButton = document.createElement("button");
                 var textButton = document.createTextNode('Enter');
                 createTable.appendChild(createButton);
                 createButton.appendChild(textButton);
                 createButton.classList.add("createButton");
 
-                createButton.onclick = function GetValueOnTable(){
+
+                createButton.onclick = function GetValueOnTable() {
                     createInputTitle.style.display = "none";
                     createdescription.style.display = "none";
                     createButton.style.display = "none";
-                    takeColumn[i].appendChild(link);
+                    addcolumns.appendChild(link);
                     link.appendChild(createTable);
                     createTable.innerHTML = '<span class="title">' + createInputTitle.value + '</span><br><span class ="descr">' + createdescription.value + '</span>';
-                    createTable.onclick = function displayPopup() {
-                            var popUp = document.querySelector('.popup');
-                            popUp.style.display = "block";
-                            popUp.innerHTML = '<a href="#"><button class="leavePopup">X</button></a><span class="task">'+ task[i].value +
-                            '</span><br><input type="text" class="titlePopUp" value="'+ createInputTitle.value +'"><br><textarea class="descriptionPopUp">'
-                            + createdescription.value+'</textarea><br><a href="#"><button class="btnChange">Change</button></a>';
 
-                        btnChange.onclick = function changeTable(){
+                    createTable.onclick = function displayPopup() {
+                        var popUp = document.querySelector('.popup');
+                        popUp.style.display = "block";
+                        popUp.innerHTML = '<a href="#"><button class="leavePopup">X</button></a><span class="task">' + task.value +
+                            '</span><br><input type="text" class="titlePopUp" value="' + createInputTitle.value + '"><br><textarea class="descriptionPopUp">'
+                            + createdescription.value + '</textarea><br><a href="#"><button class="btnChange">Change</button></a>';
+
+                        btnChange.onclick = function changeTable() {
                             console.log('ok');
                             createTable.innerHTML = "";
                             var titlePopUp = document.querySelector('.titlePopUp');
@@ -107,11 +112,11 @@ window.onload = function(){
                     };
 
 
-                    };
+                };
+            };
 
 
 
                 };
-            };
 
 };
